c43aaf7333d78a5747a70c1ee5eff307d0bec705
Thomas Frey <thomas.frey@code.berlin>
2020-03-30 15:33:17 +0200

    corrected import name & comments

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from .hello import Config
+from .config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+#Referencing Config class in config module.
 
 from app import routes
