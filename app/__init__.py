c40bf962ceae8100c47204da40856cbb171ee95b
Thomas Frey <thomas.frey@code.berlin>
2020-03-30 15:29:24 +0200

    Fixed reference 4 config (with the dot .)

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
-#from config.py import Class Config
-from config import Config
+from .hello import Config
 
 app = Flask(__name__)
-#Reference Config Class
 app.config.from_object(Config)
 
 from app import routes
