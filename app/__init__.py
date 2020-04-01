87223d3e5ae32277f8fc0000cda306a291205769
Thomas Frey <thomas.frey@code.berlin>
2020-04-01 14:49:46 +0200

    Added object for DB &  Migration Engine plus new imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 #Referencing Config class in config module.
 
-from app import routes
+#Represents Database and Migration engine
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
+#'models' defines structure of db
