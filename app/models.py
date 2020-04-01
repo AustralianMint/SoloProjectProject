0cde962931d809130204b15472c7e8a6501f0df1
Thomas Frey <thomas.frey@code.berlin>
2020-04-01 17:31:16 +0200

    typo fix

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 #User class inherits from db.Model (base class)
 class User(db.Model):
     #fields created as instances of db.Column
-    id = db.Column(db.Intiger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
