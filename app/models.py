da33400e5858641ffcd502077a3768505de5631e
Thomas Frey <thomas.frey@code.berlin>
2020-04-01 14:46:23 +0200

    User database model

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+#User class inherits from db.Model (base class)
+class User(db.Model):
+    #fields created as instances of db.Column
+    id = db.Column(db.Intiger, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    #__repr__ tells python how to print objects
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
