6ab223aff9778db38e221480a59e8d0c02475d3e
Thomas Frey <thomas.frey@code.berlin>
2020-04-01 14:51:14 +0200

    Referenced where DB is plus comments

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
 #Secret key configuration
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    #Configuration Variable storing where the db is located.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #stops signaling application when change in the db.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
