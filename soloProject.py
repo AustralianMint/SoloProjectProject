78bf516e50a919ce654aec523d2ac0051b05e9e1
Thomas Frey <thomas.frey@code.berlin>
2020-04-06 12:20:14 +0200

    added flask-shell command w/ imported db entities

--- soloProject.py
+++ soloProject.py
@@ -1,2 +1,9 @@
 #Imports the 'app' variable which is part of 'app package'.
-from app import app
+from app import app, db
+from app.models import User, Post
+
+#After 'flask shell', shell calls & loads all things in function
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
