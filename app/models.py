4b338f20059d976a03207f861ffeb3f2cbdf465d
Thomas Frey <thomas.frey@code.berlin>
2020-04-06 12:48:33 +0200

    Password hashing & verification, user mixin class

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from datetime import datetime
 from app import db
 
 #User class inherits from db.Model (base class)
-class User(db.Model):
+class User(UserMixin, db.Model):
     #fields created as instances of db.Column
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -14,6 +16,13 @@ class User(db.Model):
     #__repr__ tells python how to print objects
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
