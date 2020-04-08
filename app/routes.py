fed30ad53f7e3fb1096757b79c036cf66bf721a2
Thomas Frey <thomas.frey@code.berlin>
2020-04-08 16:46:21 +0200

    Gave the Login view sum function logit

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 
 #All decorators which invoke the function when called.
 @app.route('/')
@@ -13,12 +15,18 @@ from app.forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
 
-    #Validate on submit does form heavy lifting
+    #Checks if user exists & Password matches
+    #If correct, remembers usr, serves index.html
     if form.validate_on_submit():
-        flash('Login request for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
