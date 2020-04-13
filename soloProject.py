875a2b19e8516b0c60d62be5e2cce9c4738627a3
Thomas Frey <thomas.frey@code.berlin>
2020-04-13 18:37:24 +0200

    Imorted new model, added 2 shell

--- soloProject.py
+++ soloProject.py
@@ -1,9 +1,9 @@
 #Imports the 'app' variable which is part of 'app package'.
 from app import app, db
-from app.models import User, Post
+from app.models import User, Post, Primary_clothes
 
 #After 'flask shell', shell calls & loads all things in function
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Primary_clothes': Primary_clothes}
 
