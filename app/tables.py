50931c07f1c6add16f26a8f54c8da2f329d4eedb
Thomas Frey <thomas.frey@code.berlin>
2020-04-14 14:05:03 +0200

    table defs for other and highschool

--- app/tables.py
+++ app/tables.py
@@ -11,3 +11,15 @@ class P_Items(Table):
     id = Col('Id', show=True)
     clothingItem = Col('Clothing Item')
     quantity = Col('Quantity')
+
+#showing 'other' items.
+class Other_Items(Table):
+    id = Col('Id', show=True)
+    clothingItem = Col('Clothing Item')
+    quantity = Col('Quantity')
+
+#showing highschool items.
+class high_clothes_table(Table):
+    id = Col('Id', show=True)
+    clothingItem = Col('Clothing Item')
+    quantity = Col('Quantitiy')
